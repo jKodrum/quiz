ad438fabef7a3c480120d230c01c0b8108c50e8d
jKodrum <jkodrum@gmail.com>
2015-10-18 00:24:09 +0800

    implement iterative q27

--- q27/main.c
+++ q27/main.c
@@ -1,6 +1,43 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define CARDNUM 52
+
+int perfectRand(int lower, int upper)
+{
+    if (upper<lower) {
+        int t = lower;
+        lower = upper;
+        upper = t;
+    }
+    int range = upper - lower;
+    return (double)rand() / ((double)RAND_MAX+1) * range + lower;
+}
+
+void deal()
+{
+    int cards[CARDNUM], deck[CARDNUM];
+    // init deck
+    for (int i=0; i<CARDNUM; i++)
+        deck[i] = i;
+
+    // dealing
+    for (int i=0; i<CARDNUM; i++) {
+        int deck_i = perfectRand(0, CARDNUM-i);
+        cards[i] = deck[deck_i];
+        for (int j=deck_i; j<CARDNUM-i-1; j++)
+            deck[j] = deck[j+1];
+    }
+
+    // print cards
+    for (int i=0; i<CARDNUM; i++)
+        printf("%d ", cards[i]);
+    printf("\n");
+}
 
 int main()
 {
+    srand(time(NULL));
+    deal();
     return 0;
 }
