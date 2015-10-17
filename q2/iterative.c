742d6e85ce783ed26a93b2313df644d4562e19f9
jKodrum <jkodrum@gmail.com>
2015-10-17 19:41:26 +0800

    implement iterative q2
    This file take first argument as input file name.
    The strings of testing data has maximum length 100000,
    that's why I define MAX_LEN as 100000.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,33 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#define MAX_LEN 100000
 
-int main()
+char smallest_character(char arr[], char c)
 {
+    for (int i=0; arr[i]!='\0'; i++)
+        if (arr[i] > c)  return arr[i];
+    return arr[0];
+}
+
+int main(int argc, char *argv[])
+{
+    char str[MAX_LEN];
+    char given;
+    FILE *fp;
+    assert(fp=fopen(argv[1], "r"));
+    while (fscanf(fp,"%s %c", str, &given)==2) {
+
+        /* Print input message
+        assert(printf("Input: ['%c'", str[0]));
+        for (char *c=str+1; *c; c++)
+            printf(", '%c'", *c);
+        printf("], '%c'\n", given);
+        */
+
+        //printf("Output: '%c'\n", smallest_character(str, given));
+        printf("%c\n", smallest_character(str, given));
+    }
+
     return 0;
 }
